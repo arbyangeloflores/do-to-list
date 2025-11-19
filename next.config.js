fddb6c29b4db56b899c590095553d6114e3aa773
arbyangeloflores <arbyangelo.flores@hcdc.edu.ph>
2025-11-19 09:51:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MYSQL_HOST: process.env.MYSQL_HOST,
-    MYSQL_USER: process.env.MYSQL_USER,
-    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
-    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
-  },
-  webpack(config) {
-    // No custom webpack config needed currently
-    return config;
-  }
-};
+  trailingSlash: false,
+  // basePath: '', // no base path unless you explicitly want one
+}
 
 module.exports = nextConfig;
